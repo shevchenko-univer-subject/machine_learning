d728580d2426d6df83b50d28a51bd5992ab4be19
Andrey <shevchenko.andrey.uzurpastor@gmail.com>
2023-09-19 14:46:16 +0300

    use different ways of processing frame with NaN values

--- pr1.py
+++ pr1.py
@@ -5,4 +5,17 @@ import numpy as np
 raw_df = pandas.read_csv("./airquality.csv")
 df = pandas.DataFrame(raw_df)
 
-print(df.head())
+print(df.describe())
+
+df_with_removed_records = df.dropna()
+print("------ df_with_removed_records")
+print(df_with_removed_records.describe())
+# build correlation matrix 
+
+df_filled_with_mean = df.fillna(df.mean())
+print("------ df_filled_with_mean")
+print(df_filled_with_mean.describe())
+
+df_with_interpolated_values = df.interpolate() # default method is linear
+print("------ df_with_interpolated_values")
+print(df_with_interpolated_values.describe())
